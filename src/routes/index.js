6601c832c80a7769d85a9136e7475753243343e4
George Luis Costa Ribeiro <georgel.jc98@gmail.com>
2022-10-31 17:27:37 -0300

    ajuste rotas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,22 +1,22 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
-// import Search from './page/search_produto/search'
-// import Dia from './page/busca_do_dia/dia'
-// import Semana from './page/busca_da_semana/semana'
-import TelaLogin from './page/tela_login/telaLogin'
-// import TelaMain from './page/tela_main/telaMain'
-// import Cadastro from './page/cadastro_users/cadastro'
+import Search from '../page/search_produto/search'
+import Dia from '../page/busca_do_dia/dia'
+import Semana from '../page/busca_da_semana/semana'
+import TelaLogin from '../page/tela_login/telaLogin'
+import TelaMain from '../page/tela_main/telaMain'
+import Cadastro from '../page/cadastro_users/cadastro'
 
 export default function Routers() {
     return (
         <Routes>
             <Route exact path="/" element={<TelaLogin />} />
-            {/* <Route exact path="/Cadastro" element={<Cadastro />} />
+            <Route exact path="/Cadastro" element={<Cadastro />} />
             <Route exact path="/Main" element={<TelaMain />} />
             <Route exact path="/Dia" element={<Dia />} />
             <Route exact path="/Semana" element={<Semana />} />
-            <Route exact path="/Search" element={<Search />} /> */}
+            <Route exact path="/Search" element={<Search />} />
         </Routes>
     )
 }
