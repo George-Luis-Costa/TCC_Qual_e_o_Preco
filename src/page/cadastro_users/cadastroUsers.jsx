ae9bdb90c1cf52d72b22a4050f7022324e9c5c17
George Luis Costa Ribeiro <georgel.jc98@gmail.com>
2022-11-02 19:22:04 -0300

    Ajustes CSS (organizacao)

--- src/page/cadastro_users/cadastroUsers.jsx
+++ src/page/cadastro_users/cadastroUsers.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from "react-router-dom"
 import Card from '../../components/Card'
-import '../cadastro_users/style.css'
 import SearchIcon from '../../assets/search.svg'
 import perfil from '../../images/perfil.png'
 
