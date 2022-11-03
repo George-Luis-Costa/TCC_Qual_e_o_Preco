056759d2c53f221fffea387f84db856221a552f1
George Luis Costa Ribeiro <georgel.jc98@gmail.com>
2022-11-02 23:33:35 -0300

    Botao voltar ajustado width

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import Routes from './routes/index';
 
-
-
-
 function App() {
   return (
     <BrowserRouter>
