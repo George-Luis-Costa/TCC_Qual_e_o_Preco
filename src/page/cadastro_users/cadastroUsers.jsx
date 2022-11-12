a6d18c855e77b554cea5d865d4d6b9b3c00d4384
George Luis Costa Ribeiro <georgel.jc98@gmail.com>
2022-11-12 12:37:54 -0300

    Tests

--- src/page/cadastro_users/cadastroUsers.jsx
+++ src/page/cadastro_users/cadastroUsers.jsx
@@ -1,9 +1,22 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from "react-router-dom"
 import { toast } from 'react-toastify'
-
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as yup from "yup";
 
 const API_URL = `https://qualpreco-api.herokuapp.com/tcc-api`
+
+const validationSchema = yup.object({
+    email: yup
+        .string()
+        .email("Enter a valid email")
+        .required("Email is required"),
+    password: yup
+        .string()
+        .min(8, "Password minimum 8 characters")
+        .required("Password is required")
+});
+
 
 
 const Cadastro = () => {
@@ -12,7 +25,8 @@ const Cadastro = () => {
     const [data, setData] = useState({})
 
     function createUser(user) {
-        toastId.current = toast.loading("Buscando...")
+        toastId.current = toast.loading("Enviando...")
+
         fetch(API_URL + "/user", {
             method: 'POST',
             headers: {
@@ -53,43 +67,47 @@ const Cadastro = () => {
     }
 
     return (
-        <div className="tela-box" style={{ height: '550px' }}>
-            <h1 className="titulo_cadastro">SIGN UP</h1>
-            <br /><br />
-
-            <form onSubmit={submit} style={{ color: 'aliceblue' }} className="formulario">
-
-                <div className="inputs_cadastro">
-                    <label htmlFor="nome" className="labels_label--format"><strong>Nome:</strong></label>
-                    <br />
-                    <input type="text" name="nome" id="name" onChange={handleChange} className="input-cadastro" required />
-                    <br />
-                    <label htmlFor="sobrenome" className="labels_label--format"><strong>Sobrenome:</strong>
-                    </label>
-                    <br />
-                    <input type="text" name="sobrenome" id="last_name" onChange={handleChange} className="input-cadastro" required />
-                    <br />
-                    <label htmlFor="email" className="labels_label--format"><strong>Email:</strong>
-                    </label>
-                    <br />
-                    <input type="text" name="email" id="email" onChange={handleChange} className="input-cadastro" required />
-                    <br />
-                    <label htmlFor="password" className="labels_label--format"><strong>Senha:</strong>
-                    </label>
-                    <br />
-                    <input type="password" name="password" id="password" onChange={handleChange} className="input-cadastro" required />
-                    <br /><br />
-                </div>
-
+        <Formik
+            validationSchema={validationSchema}
+        >
+            <div className="tela-box" style={{ height: '550px' }}>
+                <h1 className="titulo_cadastro">SIGN UP</h1>
                 <br /><br />
 
-                <div className="create_account">
-                    <Link to="/" title="Voltar para o menu anterior" className="button_submit button_bg transitionBg cadastro_link--submit">Voltar</Link>
-                    <br />
-                    <input type="submit" defaultValue="Criar Conta" className="button_submit button_bg transitionBg input_submit--criarConta" title="Ao clicar seus dados serão enviados" />
-                </div>
-            </form>
-        </div>
+                <form onSubmit={submit} style={{ color: 'aliceblue' }} className="formulario">
+
+                    <div className="inputs_cadastro">
+                        <label htmlFor="nome" className="labels_label--format"><strong>Nome:</strong></label>
+                        <br />
+                        <Field type="text" name="nome" id="name" onChange={handleChange} className="input-cadastro" required />
+                        <br />
+
+                        <label htmlFor="sobrenome" className="labels_label--format"><strong>Sobrenome:</strong></label>
+                        <br />
+                        <Field type="text" name="sobrenome" id="last_name" onChange={handleChange} className="input-cadastro" required />
+                        <br />
+
+                        <label htmlFor="email" className="labels_label--format"><strong>Email:</strong></label>
+                        <br />
+                        <Field type="email" name="email" id="email" onChange={handleChange} className="input-cadastro" required />
+                        <br />
+
+                        <label htmlFor="password" className="labels_label--format"><strong>Senha:</strong></label>
+                        <br />
+                        <Field type="password" name="password" id="password" onChange={handleChange} className="input-cadastro" required />
+                        <br /><br />
+                    </div>
+
+                    <br /><br />
+
+                    <div className="create_account">
+                        <Link to="/" title="Voltar para o menu anterior" className="button_submit button_bg transitionBg cadastro_link--submit">Voltar</Link>
+                        <br />
+                        <input type="submit" defaultValue="Criar Conta" className="button_submit button_bg transitionBg input_submit--criarConta" title="Ao clicar seus dados serão enviados" />
+                    </div>
+                </form>
+            </div>
+        </Formik>
     )
 }
 
