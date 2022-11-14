fa49f7fda0e47e151b2ef930099adb37b148abba
George Luis Costa Ribeiro <georgel.jc98@gmail.com>
2022-11-13 22:02:01 -0300

    Validacao feita*

--- src/page/busca_da_semana/semana.jsx
+++ src/page/busca_da_semana/semana.jsx
@@ -24,6 +24,7 @@ const Semana = () => {
             .then((res) => res.json())
             .then((data) => {
                 setAds(data)
+                // console.log(data)
                 if (data.length === 0) {
                     // toast.info("Anúncio(s) não encontrado(s)!")
                     toast.update(toastId.current, {
--- src/page/cadastro_produto/cadastroProduto.jsx
+++ src/page/cadastro_produto/cadastroProduto.jsx
@@ -2,20 +2,33 @@ import React, { useEffect, useState } from 'react'
 import { Link } from "react-router-dom"
 import LayoutComponents from '../../components/LayoutComponents'
 import { toast } from 'react-toastify'
-import { Formik, Form, Field, ErrorMessage, useFormik } from "formik";
+import { Formik, useFormik } from "formik";
 import * as yup from 'yup';
+import { parse, isDate } from 'date-fns';
+
 
 const API_URL = `https://qualpreco-api.herokuapp.com/tcc-api`
+
+// **Validação Formulário
+const validateStyle = {
+    color: "red",
+}
+
+// const currentDate = new Date().toLocaleDateString()
+const today = new Date().toLocaleDateString()
+
+function parseDateString(value, originalValue) {
+    const parsedDate = isDate(originalValue)
+        ? originalValue
+        : parse(originalValue, "dd/mm/yyyy", new Date());
+
+    return parsedDate;
+}
+// **Validação Formulário
 
 export const CadastroProduto = () => {
 
     const toastId = React.useRef(null)
-
-    // const [name, setName] = useState("");
-    // const [marca, setMarca] = useState("");
-    // const [data, setData] = useState("");
-    // const [endereco, setEndereco] = useState("");
-    // const [preco, setPreco] = useState("");
 
     const formik = useFormik({
         initialValues: {
@@ -27,19 +40,30 @@ export const CadastroProduto = () => {
         },
         validationSchema: yup.object({
             name: yup.string()
-                .max(15, 'Must be 15 characters or less')
-                .required('Required'),
+                .max(30, '*No máximo 30 caracteres!')
+                .required('*Obrigatório'),
+
             brand: yup.string()
-                .max(20, 'Must be 20 characters or less')
-                .required('Required'),
-            //data: yup.string().email('Invalid email address').required('Required'),
+                .max(20, '*No máximo 20 caracteres!'),
+
+            post_date: yup.date()
+                .transform(parseDateString)
+                .max(today, `*Por favor, no máximo a data atual: ${today} `)
+                .required('*Obrigatório').typeError("*Por favor coloque a data no formato dd/mm/aa"),
+
+            address: yup.string()
+                .max(45, 'No máximo 45 caracteres!')
+                .required("*Obrigatório"),
+
+            price: yup.number().max(1000, "*No máximo 4 dígitos (6 algarismos -> 1000.00").required("*Obrigatório").typeError("*Por favor coloque um número Válido. No máximo 4 dígitos (6 algarismos -> 1000.00"),
+
         }),
         onSubmit: values => {
-          alert(JSON.stringify(values, null, 2));
-          console.log(values)
-          createProduct(values)
+            // alert(JSON.stringify(values, null, 2));
+            // console.log(values)
+            createProduct(values)
         },
-      });
+    });
 
     function createProduct(product) {
         toastId.current = toast.loading("Buscando...")
@@ -65,137 +89,120 @@ export const CadastroProduto = () => {
             })
     }
 
-    // function resetar() {
-    //     setName("")
-    //     setMarca("")
-    //     setData("")
-    //     setEndereco("")
-    //     setPreco("")
-    //     // alert("Os Campos do Formulário foram Resetados!")
-    //     toast.success("Os campos do formulário foram resetados!")
-    // }
+    function resetar() {
+        formik.resetForm()
+        // alert("Os Campos do Formulário foram Resetados!")
+        toast.success("Os campos do formulário foram resetados!")
+    }
 
-    // const submit = (e) => {
-    //     const product = {
-    //         "name": name,
-    //         "brand": marca,
-    //         "price": preco,
-    //         "address": endereco,
-    //         "post_date": data,
-    //     }
-    //     e.preventDefault()
-    //     // createProduct(product)
-    //     setName("")
-    //     setMarca("")
-    //     setPreco("")
-    // }
-    /* onSubmit={submit} */
     return (
         <LayoutComponents>
-                    <form className="login-form" onSubmit={formik.handleSubmit}>
-                        <span className="login-form-title"> Adicionar Produto </span>
-                        <br />
-                        {/* <span className="login-form-title">
+            <form className="login-form" onSubmit={formik.handleSubmit}>
+                <span className="login-form-title"> Adicionar Produto </span>
+                <br />
+                {/* <span className="login-form-title">
                         <img src={jpIMG} alt="Jovem Programador" />
                         </span> */}
 
-                        <div className="wrap-input">
-                            <input
-                                id="name"
-                                name='name'
-                                className={formik.values.name !== "" ? "has-val input" : "input"}
-                                type="text"
-                                value={formik.values.name}
-                                onBlur={formik.handleBlur}
-                                onChange={formik.handleChange}
-                            />
-                            {formik.touched.name && formik.errors.name ? (
-                                <div>{formik.errors.name}</div>
-                            ) : null}
-                            <span className="focus-input" data-placeholder="Nome..."></span>
-                        </div>
+                <div className="wrap-input">
+                    <input
+                        id="name"
+                        name='name'
+                        className={formik.values.name !== "" ? "has-val input" : "input"}
+                        type="text"
+                        value={formik.values.name}
+                        onBlur={formik.handleBlur}
+                        onChange={formik.handleChange}
+                        required
+                    />
+                    <span className="focus-input" data-placeholder="Nome..."></span>
+                    {formik.touched.name && formik.errors.name ? (
+                        <div style={validateStyle}>{formik.errors.name}</div>
+                    ) : null}
+                </div>
 
-                        <div className="wrap-input">
-                            <input
-                                id="brand"
-                                name='brand'
-                                className={formik.values.brand !== "" ? "has-val input" : "input"}
-                                type="text"
-                                value={formik.values.brand}
-                                onBlur={formik.handleBlur}
-                                onChange={formik.handleChange}
-                            />
-                            {formik.touched.brand && formik.errors.brand ? (
-                                <div>{formik.errors.brand}</div>
-                            ) : null}
-                            <span className="focus-input" data-placeholder="Marca..."></span>
-                        </div>
+                <div className="wrap-input">
+                    <input
+                        id="brand"
+                        name='brand'
+                        className={formik.values.brand !== "" ? "has-val input" : "input"}
+                        type="text"
+                        value={formik.values.brand}
+                        onBlur={formik.handleBlur}
+                        onChange={formik.handleChange}
+                    />
+                    <span className="focus-input" data-placeholder="Marca..."></span>
+                    {formik.touched.brand && formik.errors.brand ? (
+                        <div style={validateStyle}>{formik.errors.brand}</div>
+                    ) : null}
+                </div>
 
-                        <div className="wrap-input">
-                            <input
-                                id="post_date"
-                                name='post_date'
-                                className={formik.values.post_date !== "" ? "has-val input" : "input"}
-                                type="text"
-                                value={formik.values.post_date}
-                                onBlur={formik.handleBlur}
-                                onChange={formik.handleChange}
-                            />
-                            {formik.touched.post_date && formik.errors.post_date ? (
-                                <div>{formik.errors.post_date}</div>
-                            ) : null}
-                            <span className="focus-input" data-placeholder="Data..."></span>
-                        </div>
+                <div className="wrap-input">
+                    <input
+                        id="post_date"
+                        name='post_date'
+                        className={formik.values.post_date !== "" ? "has-val input" : "input"}
+                        type="text"
+                        value={formik.values.post_date}
+                        onBlur={formik.handleBlur}
+                        onChange={formik.handleChange}
+                        required
+                    />
+                    <span className="focus-input" data-placeholder="Data..."></span>
+                    {formik.touched.post_date && formik.errors.post_date ? (
+                        <div style={validateStyle}>{formik.errors.post_date}</div>
+                    ) : null}
+                </div>
 
-                        <div className="wrap-input">
-                            <input
-                                id="address"
-                                name='address'
-                                className={formik.values.address !== "" ? "has-val input" : "input"}
-                                type="text"
-                                value={formik.values.address}
-                                onBlur={formik.handleBlur}
-                                onChange={formik.handleChange}
-                            />
-                            {formik.touched.address && formik.errors.address ? (
-                                <div>{formik.errors.address}</div>
-                            ) : null}
-                            <span className="focus-input" data-placeholder="Endereço..."></span>
-                        </div>
+                <div className="wrap-input">
+                    <input
+                        id="address"
+                        name='address'
+                        className={formik.values.address !== "" ? "has-val input" : "input"}
+                        type="text"
+                        value={formik.values.address}
+                        onBlur={formik.handleBlur}
+                        onChange={formik.handleChange}
+                        required
+                    />
+                    <span className="focus-input" data-placeholder="Endereço..."></span>
+                    {formik.touched.address && formik.errors.address ? (
+                        <div style={validateStyle}>{formik.errors.address}</div>
+                    ) : null}
+                </div>
 
-                        <div className="wrap-input">
-                            <input
-                                id="price"
-                                name='price'
-                                className={formik.values.price !== "" ? "has-val input" : "input"}
-                                type="text"
-                                value={formik.values.price}
-                                onBlur={formik.handleBlur}
-                                onChange={formik.handleChange}
-                            />
-                            {formik.touched.price && formik.errors.price ? (
-                                <div>{formik.errors.price}</div>
-                            ) : null}
-                            <span className="focus-input" data-placeholder="Preço..."></span>
-                        </div>
+                <div className="wrap-input">
+                    <input
+                        id="price"
+                        name='price'
+                        className={formik.values.price !== "" ? "has-val input" : "input"}
+                        type="text"
+                        value={formik.values.price}
+                        onBlur={formik.handleBlur}
+                        onChange={formik.handleChange}
+                        required
+                    />
+                    <span className="focus-input" data-placeholder="Preço..."></span>
+                    {formik.touched.price && formik.errors.price ? (
+                        <div style={validateStyle}>{formik.errors.price}</div>
+                    ) : null}
+                </div>
 
-                        <div className="container-login-form-btn">
-                            <button type='submit' className="login-form-btn button_submit">Enviar</button>
-                        </div>
+                <div className="container-login-form-btn">
+                    <button type='submit' className="login-form-btn button_submit">Enviar</button>
+                </div>
 
+                <div className="container-login-form-btn">
+                    <button type='reset' className="login-form-btn button_submit" onClick={resetar} >Reset</button>
+                </div>
 
+            </form>
 
-                        {/* <div className="login-form-btn">
-                            <button type='button' className="login-form-btn button_submit" >Reset</button>
-                        </div> */}
-                    </form>
-
-                    <Link to="/Main" >
-                            <div className="container-login-form-btn">
-                                <button type='button' className="login-form-btn button_submit">Voltar</button>
-                            </div>
-                        </Link>
-
+            <Link to="/Main" >
+                <div className="container-login-form-btn">
+                    <button type='button' className="login-form-btn button_submit">Voltar</button>
+                </div>
+            </Link>
         </LayoutComponents >
     )
 }
